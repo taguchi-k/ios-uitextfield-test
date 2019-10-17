d2ce72080bb34cebe4fbcf6433ec88ce908e3999
kentaro <taguchi-k@users.noreply.github.com>
2019-10-17 22:29:24 +0900

    Add IBOutlet

---
+++ ios-uitextfield-test/ios-uitextfield-test/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ios-uitextfield-test
+//
+//  Created by Kentaro on 2019/10/17.
+//  Copyright © 2019 Kentao Taguchi. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - IBOutlet
+
+    @IBOutlet private weak var lastNameTextField: UITextField!
+
+    @IBOutlet private weak var firstNameTextField: UITextField!
+
+    @IBOutlet private weak var button: UIButton!
+
+    // MARK: - Property
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
