4b3728726049271b668d9542d1c9fe1d4bac02e8
kentaro <taguchi-k@users.noreply.github.com>
2019-10-18 08:03:05 +0900

    Add Methods

--- ios-uitextfield-test/ios-uitextfield-test/ViewController.swift
+++ ios-uitextfield-test/ios-uitextfield-test/ViewController.swift
@@ -18,12 +18,42 @@ final class ViewController: UIViewController {
 
     @IBOutlet private weak var button: UIButton!
 
-    // MARK: - Property
-
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private
+
+    private func setup() {
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(textDidChange),
+            name: UITextField.textDidChangeNotification,
+            object: nil)
+    }
+
+    @objc private func textDidChange(sender: Notification) {
+        guard let textField = sender.object as? UITextField else { return }
+        trimText(textField)
+        updateButtonEnabled()
+    }
+
+    private func trimText(_ textField: UITextField) {
+
+        let trimedText = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        textField.text = trimedText
+    }
+
+    private func updateButtonEnabled() {
+        button.isEnabled = validateNames()
+    }
+
+    private func validateNames() -> Bool {
+        !(lastNameTextField.text?.isEmpty == true
+            && firstNameTextField.text?.isEmpty == true)
     }
 }
-
